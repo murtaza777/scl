5cf626f5fde455c3384885b4d46a9f36b2eba53e
murtaza <no_fear_m2000@yahoo.com>
2017-12-23 13:10:03 +1100

    Added port cleanup

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ const mongo = require('./config/mongo');
 
 const app = express();
 
-const port = 3000;
+const port = process.env.PORT || 3000;
+
 
 app.use(bodyParser.urlencoded({extended: true}));
 
