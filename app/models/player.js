c8ad21809ce188a93d1439ab5e3740d442048614
murtaza <no_fear_m2000@yahoo.com>
2017-12-27 23:10:29 +1100

    Added player model and added partials to players.ejs

---
+++ app/models/player.js
@@ -0,0 +1,30 @@
+var mongoose = require('mongoose');
+// var bcrypt   = require('bcrypt-nodejs');
+
+// define the schema for our user model
+var playerSchema = mongoose.Schema({
+
+    name     : String,
+    email    : String,
+    team     : String,
+    phone    : Number,
+    bat      : { type: Number, min: 1, max: 10, default: 0 },
+    bowl     : { type: Number, min: 1, max: 10, default: 0 },
+    wk       : { type: Number, min: 1, max: 10, default: 0 },
+    lead     : Boolean,
+    round    : { type: Number, min: 1, max: 2, default: 0 },
+    bids     : [
+        {
+            team: String,
+            amount: { type: Number, default: 0 }
+        }
+    ],
+    price    : { type: Number, default: 0 }
+
+});
+
+// methods ======================
+
+
+// create the model for users and expose it to our app
+module.exports = mongoose.model('Player', playerSchema);
